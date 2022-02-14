564b82cff395b9dac38410108357faf88b49511b
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-02-14 11:51:43 -0800

    Create Counting-Sort-1.py

---
+++ Counting-Sort-1.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    freq = [0] * 100
+    for i in range(len(arr)):
+        freq[arr[i]] += 1
+    return freq
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
