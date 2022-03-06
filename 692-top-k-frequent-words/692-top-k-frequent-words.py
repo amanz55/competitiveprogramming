2531c26d5b5b86d6d19538a0e1251191bea7605a
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-03-06 21:00:26 +0300

    Time: 72 ms (63.39%), Space: 14 MB (57.41%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -2,9 +2,9 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         mydict = Counter(words)
         myheap = []
-        for key, value in mydict.items():
-            heappush(myheap, (-1 * value, key))
+        for key in mydict:
+            heappush(myheap, (-1 * mydict[key], key))
         res = []
         for i in range(k):
             res.append(heappop(myheap)[1])
-        return res        
+        return res 
