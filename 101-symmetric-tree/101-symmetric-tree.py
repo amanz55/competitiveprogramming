acee9b4e30b25882bd6ec6cf2b3d7579c9f12eb8
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-03-09 16:01:03 +0300

    Time: 47 ms (56.51%), Space: 14 MB (50.88%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,55 +7,107 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        traversedleft = []
-        traversedright = []
-        levelLeft = 1
-        levelright = 1
+        myque = deque([root])
+        level = 0
         
-        def gotoleft(r1,levelleft):
-            if r1 is None:
-                return levelleft
-            levelleft += 1
-            return gotoleft(r1.left,levelleft)
+        while myque:
+            size = len(myque)
+            for i in range(size):
+                rt = myque.popleft()
+                if rt:
+                    myque.append(rt.left)
+                    myque.append(rt.right)
+                else:
+                    continue
             
-        def gotoright(r2,levelright):
-            if r2 is None:
-                return levelright
-            levelright += 1
-            return gotoright(r2.right,levelright)
-        
-        if gotoleft(root,levelLeft) != gotoright(root,levelright):
-            return False
-        
-        def traverse(r):
-            if r is None:
-                return
+            start, end = 0, len(myque) - 1
+            while start < end:
+                if myque[start] == None or myque[end] == None:
+                    if myque[start] == myque[end] == None:
+                        start += 1
+                        end -= 1
+                        continue
+                    else:
+                        return False
+                elif myque[start].val != myque[end].val:
+                    return False
+                start += 1
+                end -= 1
+                
+        return True
             
-            traverse(r.left)
-        
-            traversedleft.append(r.val)
             
-            traverse(r.right)
-        
-        def travers(r):
-            if r is None:
-                return
             
-            travers(r.left)
         
-            traversedright.append(r.val)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         traversedleft = []
+#         traversedright = []
+#         levelLeft = 1
+#         levelright = 1
+        
+#         def gotoleft(r1,levelleft):
+#             if r1 is None:
+#                 return levelleft
+#             levelleft += 1
+#             return gotoleft(r1.left,levelleft)
             
-            travers(r.right)
+#         def gotoright(r2,levelright):
+#             if r2 is None:
+#                 return levelright
+#             levelright += 1
+#             return gotoright(r2.right,levelright)
+        
+#         if gotoleft(root,levelLeft) != gotoright(root,levelright):
+#             return False
+        
+#         def traverse(r):
+#             if r is None:
+#                 return
+            
+#             traverse(r.left)
+        
+#             traversedleft.append(r.val)
+            
+#             traverse(r.right)
+        
+#         def travers(r):
+#             if r is None:
+#                 return
+            
+#             travers(r.left)
+        
+#             traversedright.append(r.val)
+            
+#             travers(r.right)
               
-        traverse(root.left)
-        travers(root.right)
-        print(traversedleft)
-        print(traversedright)
+#         traverse(root.left)
+#         travers(root.right)
+#         print(traversedleft)
+#         print(traversedright)
         
-        if list(reversed(traversedleft)) == traversedright:
-            return True
-        else:
-            return False
+#         if list(reversed(traversedleft)) == traversedright:
+#             return True
+#         else:
+#             return False
 
 
         
