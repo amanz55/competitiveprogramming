456cb9404200c1d07c802626eb04d64ab2f7e467
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-03-10 12:09:55 +0300

    Time: 144 ms (57.62%), Space: 17.8 MB (66.90%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -20,3 +20,18 @@ class Solution:
         return dfs(root, None, None, 0)
         
         
+        
+#         self.sums = 0
+        
+#         def dfs(rt, parent, grand):
+#             if not rt:
+#                 return self.sums
+#             if grand and grand.val % 2 == 0:
+#                 self.sums += rt.val
+                
+#             dfs(rt.left, rt, parent)
+#             dfs(rt.right, rt, parent)
+            
+#         dfs(root, None, None)
+#         return self.sums
+        
