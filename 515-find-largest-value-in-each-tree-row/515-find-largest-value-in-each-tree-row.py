3b35cfc85a50c7a1ccd860eb53c38113ff8932db
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-03-24 23:15:44 +0300

    Time: 40 ms (98.91%), Space: 16.2 MB (97.14%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.py
@@ -4,32 +4,86 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
-    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        myque = deque([root])
-        if root:
-            result = [root.val]
-        else:
+    def largestValues(self, root: TreeNode) -> List[int]:
+        arr = []
+        if not root:
             return []
         
-        while myque:
-            size = len(myque)
+        queue = deque([root])
+        while queue:
+            max_elt = -float('inf')
+            size = len(queue)
             for i in range(size):
-                rt = myque.popleft()
-                if rt.left and rt.right:
-                    myque.append(rt.left)
-                    myque.append(rt.right)
-                elif rt.left:
-                    myque.append(rt.left)
-                elif rt.right:
-                    myque.append(rt.right)
-                else:
-                    continue
-            if len(myque) != 0:
-                maxim = myque[0].val
-                for i in myque:
-                    if i.val > maxim:
-                        maxim = i.val
-                result.append(maxim)
+                cur_elt = queue.popleft()
+                max_elt = max(max_elt, cur_elt.val)
+                if cur_elt.left:
+                    queue.append(cur_elt.left)
+                if cur_elt.right:
+                    queue.append(cur_elt.right)
+            arr.append(max_elt)
+            
+        return arr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+#         myque = deque([root])
+#         if root:
+#             result = [root.val]
+#         else:
+#             return []
+        
+#         while myque:
+#             size = len(myque)
+#             for i in range(size):
+#                 rt = myque.popleft()
+#                 if rt.left and rt.right:
+#                     myque.append(rt.left)
+#                     myque.append(rt.right)
+#                 elif rt.left:
+#                     myque.append(rt.left)
+#                 elif rt.right:
+#                     myque.append(rt.right)
+#                 else:
+#                     continue
+#             if len(myque) != 0:
+#                 maxim = myque[0].val
+#                 for i in myque:
+#                     if i.val > maxim:
+#                         maxim = i.val
+#                 result.append(maxim)
                 
-        return result
+#         return result
