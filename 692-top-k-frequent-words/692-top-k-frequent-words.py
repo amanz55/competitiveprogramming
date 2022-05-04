4a11b1215dc9c4c9c295b9ce6c3325e08115fed7
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-05-04 19:34:09 +0300

    Time: 120 ms (6.89%), Space: 14 MB (66.47%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,10 +1,32 @@
+class Node:
+    def __init__(self):
+        self.freq = 1
+        self.children = {}
+
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        mydict = Counter(words)
-        myheap = []
-        for key in mydict:
-            heappush(myheap, (-1 * mydict[key], key))
+        root = Node()
+        for i in words:
+            if i in root.children:
+                root.children[i].freq += 1
+            else:
+                root.children[i] = Node()
+        words = []
+        for i in root.children:
+            words.append([i,root.children[i].freq])
+        words.sort(key=lambda x: x[1], reverse=True)
+        result =[]
         res = []
-        for i in range(k):
-            res.append(heappop(myheap)[1])
-        return res 
+        print(words)
+        for i in range(len(words)):
+            if len(res) == 0 or words[i][1] == res[-1][1]:
+                res.append(words[i])
+            else:
+                result.extend(sorted(res))
+                res = [words[i]]
+        result.extend(sorted(res))
+        for i in range(len(result)):
+            result[i] = result[i][0]
+        return result[:k]
