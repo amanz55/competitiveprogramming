277533cb7ab773daa29ada644ca6d6ebd5a18818
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-08-08 01:19:26 -0800

    Time: 47 ms (53.78%), Space: 14 MB (31.70%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        digits = list(map(int, digits))
+        result = []
+        mapp = {
+            2 : ["a", "b", "c"],
+            3 : ["d", "e", "f"],
+            4 : ["g", "h", "i"],
+            5 : ["j", "k", "l"],
+            6 : ["m", "n", "o"],
+            7 : ["p", "q", "r", "s"],
+            8 : ["t", "u", "v"],
+            9 : ["w", "x", "y", "z"]
+        }
+        
+        def dfs(num, word):
+            num2 = digits[num]
+            for child in mapp[num2]:
+                if num == (len(digits) - 1):
+                    result.append(word+child)
+                else:
+                    dfs(num + 1, word + child)
+                        
+        if digits:dfs(0, "")
+        
+        return result
+            
