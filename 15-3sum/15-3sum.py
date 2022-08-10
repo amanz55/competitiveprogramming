e24e9c8824b26c6917b075cdf8117faa06aa16d9
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-08-10 00:36:12 -0800

    Time: 1527 ms (42.35%), Space: 18.1 MB (72.41%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -18,7 +18,7 @@ class Solution:
                         r -= 1
                         while l < r and nums[l] == nums[l-1]:
                             l += 1
-                        # while l < r and nums[r] == nums[r+1]:
-                        #     r -= 1
+                        while l < r and nums[r] == nums[r+1]:
+                            r -= 1
 
         return res
