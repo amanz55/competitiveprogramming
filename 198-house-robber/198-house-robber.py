2172e360d30a13c48542a0446f642677e9028a33
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-08-24 05:13:36 -0800

    Time: 31 ms (95.48%), Space: 13.8 MB (66.15%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,72 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+        #edges here
+        if len(nums) < 3:
+            return max(nums)
         
-        ptr1 = nums[0]
-        ptr2 = max(nums[0], nums[1])
+        dp = {}
+        
+        dp[0] = nums[0]
+        dp[1] = nums[1]
         
         for i in range(2, len(nums)):
-            ptr1 , ptr2 = ptr2 , max(ptr2 , ptr1 + nums[i])
+            j = i - 2
+            maxi = -1
+            while j >= 0:
+                maxi = max(maxi, dp[j])
+                j -= 1
             
-        return ptr2
+            if dp[i-1] > maxi + nums[i]:
+                dp[i] = dp[i-1]
+            else:
+                dp[i] = maxi + nums[i]
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        """
-        
-        1 = nums[0]
-        2 = max(nums[0], nums[1])
-        
-        ptr1 = 1
-        ptr2 = 2
-        
-        temp = ptr1
-        ptr1 = ptr2
-        ptr2 = max(ptr2, nums[i] + temp)
-        
-        
-        3 = max(nums[1], nums[0] + nums[2])
-        4 = max(nums[0] + nums[2], nums[0] + nums[3], nums[1] + nums[3])
-        
-        
-        5, 2, 1, 5
-        
-        ptr1 = 5
-        ptr2 = 5
-        
-        i = 2
-            temp = 5
-            ptr1 = 5
-            ptr2 = max(5, 6) = 6
-        i = 3
-            temp = 5
-            ptr1 = 6
-            ptr2 = max(6, 10) = 10
-        
-        """
+        return dp[len(nums) - 1]
+            
