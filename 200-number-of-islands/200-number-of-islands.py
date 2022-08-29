5e28a0d4940e9f4360979744872bdc5effc2dd3d
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-08-28 23:44:03 -0800

    Time: 452 ms (60.66%), Space: 25.2 MB (11.40%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,38 +1,23 @@
-from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
-        directions = [[0,-1], [-1,0], [0,1], [1,0]]
-        islands = 0
-        myque = deque([])
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+        inbound = lambda x,y : 0 <= x < len(grid) and 0 <= y < len(grid[0])
+        visited =set()
+        count = 0
         
-        inbound = lambda row, col : 0 <= row < len(grid) and 0 <= col < len(grid[0])
-        
-        def bfs(a, b):
-            myque.append((a,b))
-            visited.add((a,b))
-
-            while myque:
-                size = len(myque)
-
-                for i in range(size):
-                    loc = myque.popleft()
-
-                    for i in directions:
-                        x, y = loc[0] + i[0], loc[1] + i[1]
-                        if inbound(x, y) and grid[x][y] == "1" and (x, y) not in visited:
-                            bfs(x, y)
-            
-        
-        
+        def dfs(r,c):
+            visited.add((r,c))
+            for direction in directions:
+                nr, nc = r + direction[0], c + direction[1]
+                if inbound(nr,nc) and (nr,nc) not in visited and grid[nr][nc] == "1":
+                    dfs(nr,nc)
+                    
         for i in range(len(grid)):
-            
             for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    if (i, j) not in visited:
-                        islands += 1
-                        bfs(i, j)
-                        
-        return islands
-                
+                if grid[i][j]=="1" and (i,j) not in visited:
+                    count += 1
+                    dfs(i,j)
+        return count
+                    
+            
         
