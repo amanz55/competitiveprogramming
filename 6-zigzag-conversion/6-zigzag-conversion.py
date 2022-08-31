ff0f9b09bd9a02b7e3ae520a5c33115dcb8af3e3
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2022-08-31 07:17:36 -0800

    Time: 98 ms (51.09%), Space: 14.1 MB (28.30%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -0,0 +1,32 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        answer = []
+        s = list(s)
+        for i in range(numRows):
+            answer.append([])  
+        flag = True
+        idx = 0
+        l = len(s)
+        while idx < l:
+                for j in range(len(answer)):
+                    if idx < l:
+                        answer[j].append(s[idx])
+                        idx += 1
+                for k in range(len(answer) - 2, -1, -1):
+                    if idx < l:
+                        answer[k].append(s[idx])
+                        idx += 1
+                for o in range(1, len(answer), 1):
+                    if idx < l:
+                        answer[o].append(s[idx])
+                        idx += 1
+                for k in range(len(answer) - 2, 0, -1):
+                    if idx < l:
+                        answer[k].append(s[idx])
+                        idx += 1
+        for i in range(len(answer)):
+            answer[i] = "".join(answer[i])
+        return "".join(answer)
+            
+        
+        
