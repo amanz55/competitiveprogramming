1c5cc93aa33ac71aa9a456c2fc38902582469094
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2023-02-01 22:28:52 +0300

    Time: 1809 ms (71.33%), Space: 14.3 MB (51.75%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -2,21 +2,14 @@ class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
         num_players = len(scores)
         age_score = list(zip(ages, scores))
-        
         age_score.sort()
+        
         dp = [0 for i in range(num_players)]
-        dp[0] = age_score[0][1]
-        
-        ans = dp[0]
-        for i in range(1, num_players):
-            dp[i] = dp[i] + age_score[i][1]
-            for j in range(i):
-                same_age = (age_score[i][0] == age_score[j][0])
-                has_conflict = (age_score[i][1] < age_score[j][1])
-                
-                if same_age or not has_conflict:
-                    dp[i] = max(dp[j] + age_score[i][1], dp[i])
-                    
-            ans = max(dp[i], ans)
-        
-        return ans
+        dp[-1] = age_score[-1][1]
+        for i in range(num_players - 2, -1, -1):
+            temp_max = 0
+            for j in range(i + 1, num_players):
+                if age_score[j][1] >= age_score[i][1]:
+                    temp_max = max(temp_max, dp[j])
+            dp[i] = age_score[i][1] + temp_max
+        return max(dp)
