b8d9f26db365a13a7fc66500826ea03cabbf529d
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2023-02-08 12:34:28 +0300

    Time: 1128 ms (82.24%), Space: 62.6 MB (78.51%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -6,18 +6,19 @@ class Solution:
             pairs[src].append(dest)
             pairs[dest].append(src)
         start = -inf 
+        
         for key in pairs:
             if len(pairs[key]) == 1:
                 start = key
                 break
                 
-        answer = [start]
-        visited = set()
-        visited.add(start)
-        for _ in range(len(pairs)):
-            for dest in pairs[answer[-1]]:
-                if dest not in visited:
-                    visited.add(dest)
-                    answer.append(dest)
+        answer = [start, pairs[start][0]]
+        
+        for _ in range(len(pairs) - 2):
+            dest = pairs[answer[-1]]
+            if dest[0] != answer[-2]:
+                answer.append(dest[0])
+            else:
+                answer.append(dest[1])
                 
         return answer
