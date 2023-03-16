58c5cecc5c78b3ef1b4a221bbe9227c0b15b3988
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2023-03-16 10:39:04 +0300

    Time: 36 ms (69.68%), Space: 13.8 MB (61.50%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
@@ -4,36 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+"""
+
+Author: Amanuel Alemayehu
+Description: this is a code template that can also be used to get the levels and nodes in the specific levels as a level order traversal.
+
+"""
 class Solution:
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        queue = deque()
+        queue.append(root)
+        level = 0
+        answer =[]
+        flag = True
         
-        def solve(node, height):
-            if not node:
-                return height
-            
-            right = solve(node.right, height + 1)
-            
-            if self.max_height > right :
-                self.ans = False
-            
-            
-            left = solve(node.left, height + 1)
-            
-            if self.max_height > left:
-                self.ans = False
-            
-            self.max_height = max(self.max_height, left, right)
-            self.min_height = min(self.min_height, left, right)
-            
-            if abs(self.min_height - self.max_height) > 1:
-                self.ans = False
-            
-            
-            return left
         
-        self.ans = True
-        self.max_height = -1
-        self.min_height = 101
+        while queue:
+            length = len(queue)
+            temp = []
+            for i in range(length):
+                node = queue.popleft()
+                temp.append(node.val)
+                if (not node.left and node.right) or (not flag and node.left):
+                    return False
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                else:
+                    flag = False
+            level += 1
+            answer.append([temp,level])
         
-        solve(root, 0)
-        return self.ans
+        return True
+        
