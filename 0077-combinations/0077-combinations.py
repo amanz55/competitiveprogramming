4824d71a0da8355d5c46074c4ae1d69e599bc38b
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2023-04-05 10:55:57 +0300

    Time: 881 ms (5.03%), Space: 16.1 MB (14.64%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,18 +1,24 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        nums = []
+        for i in range(1, n + 1):
+            nums.append(i)
+        visited = 0
+        def checkIfVisited(i, visited):
+            return visited & 1 << len(nums) - i - 1
         
-        answer = []
-        
-        def backtrack(num, list_, k, n):
-            if len(list_) == k:
-                answer.append(list_)
+        def backtrack(idx, path = []):
+            nonlocal visited
+            if len(path) == k:
+                ans.append(path[:])
                 return 
-            
-            if len(list_) > k or num > n:
-                return
-            
-            for idx in range(num + 1, n + 1):
-                backtrack(idx, list_ + [idx], k, n)
-                
-        backtrack(0, [], k, n)
-        return answer
+            for j in range(idx + 1, len(nums)):
+                if not checkIfVisited(j, visited):
+                    path.append(nums[j])
+                    visited = visited | 1 << len(nums) - j - 1
+                    backtrack(j, path)
+                    visited = visited ^ 1 << len(nums) - j - 1
+                    path.pop()
+        ans = []
+        backtrack(-1)
+        return ans
