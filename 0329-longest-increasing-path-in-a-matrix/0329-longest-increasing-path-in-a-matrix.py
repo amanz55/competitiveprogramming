7f17ab1d23c4e81a7b8130304aebfa091cc5ef01
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2023-04-18 01:49:07 +0300

    Time: 541 ms (25.48%), Space: 14.8 MB (85.99%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,17 +1,18 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         answer = 0
-        memo = {}
+        memo = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
         directions = [[0,1],[1,0],[0,-1],[-1,0]]
         inbound = lambda x, y: 0 <= x < len(matrix) and 0 <= y < len(matrix[0])
         
         
         def dfs(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
             
             if not inbound(i, j):
                 return 0
+            
+            if memo[i][j]:
+                return memo[i][j]
             
             maximum = 0
             
@@ -21,7 +22,7 @@ class Solution:
                 if not inbound(row, col) or matrix[row][col] > matrix[i][j]:
                     maximum = max(maximum, dfs(row, col))
                     
-            memo[(i, j)] = maximum + 1  
+            memo[i][j] = maximum + 1  
             return maximum + 1
         
         for i in range(len(matrix)):
