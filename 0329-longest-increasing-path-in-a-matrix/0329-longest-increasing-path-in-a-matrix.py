dc6664466116990734d52be331417bd2316c487b
Amanuel Alemayehu <amanuelalemayehu449@gmail.com>
2023-04-18 01:42:17 +0300

    Time: 555 ms (23.19%), Space: 19.7 MB (12.90%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,35 +1,31 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         answer = 0
-        visitedd = set()
         memo = {}
         directions = [[0,1],[1,0],[0,-1],[-1,0]]
         inbound = lambda x, y: 0 <= x < len(matrix) and 0 <= y < len(matrix[0])
         
         
-        def dfs(i, j, visited):
+        def dfs(i, j):
             if (i, j) in memo:
                 return memo[(i, j)]
             
             if not inbound(i, j):
                 return 0
             
-            visited.add((i, j))
             maximum = 0
             
             for r, c in directions:
                 row, col = i + r, j + c
                 
-                if not inbound(row, col) or ( (row, col) not in visited and matrix[row][col] > matrix[i][j]):
-                    maximum = max(maximum, dfs(row, col, visited))
+                if not inbound(row, col) or matrix[row][col] > matrix[i][j]:
+                    maximum = max(maximum, dfs(row, col))
                     
             memo[(i, j)] = maximum + 1  
-            visited.remove((i, j))
             return maximum + 1
         
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                answer = max(answer, dfs(i, j, visitedd))
-                visitedd = set()
+                answer = max(answer, dfs(i, j))
                 
         return answer
